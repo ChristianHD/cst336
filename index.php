7e54f6be458ebb7a5acbc65ef4fa245ed19b354c
Christian Guerrero <cguerrero@csumb.edu>
2018-07-08 06:58:32 +0000

    Hello world

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo "Christian Guerrero's CST336 Internet Programming";
 ?>
     <br/><br/>
         <h1 Labs </h1>
-            <a href="cst336/labs/PersonalPortfolio/index.html"> Lab 1 - Personal Portfolio </a>
+            <a href="labs/PersonalPortfolio/index.html"> Lab 1 - Personal Portfolio </a>
             
         
 </body>
