c1646a1ea55b7f3b5631c3dd0df397e1fdef55d1
Christian Guerrero <cguerrero@csumb.edu>
2018-10-30 19:44:12 +0000

    homework 1 upload

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ echo "Christian Guerrero's CST336 Internet Programming";
         <h2 Labs </h2>
             
             <a href="labs/lab1/index.html"> Lab 1 - Personal Portfolio </a><br>
-            <a href="hwk/hwk1/index.html"> Hwk1 - Operating Systems </a>
+            <a href="hwk/hwk1/index.html"> Hwk1 - Operating Systems </a><br>
+            <a href="labs/777/index.php"> Lab 2 - Slot Machine </a><br>
+            
         
 </body>
 </html>
