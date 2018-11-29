52b67dd28ee34784222fc5783b93549364633a10
Christian Guerrero <cguerrero@csumb.edu>
2018-11-29 05:21:47 +0000

    hi

--- labs/lab7/js/hangman.js
+++ labs/lab7/js/hangman.js
@@ -3,7 +3,10 @@ var selectedWord = "";
 var selectedHint = "";
 var board = [];
 var remainingGuesses = 6;
-var words = ["snake", "monkey", "beetle"];
+var words = [{ word: "snake", hint: "It's a reptile"},
+             { word: "monkey", hint: "It's a mammal"},
+             { word: "bettle", hint: "It's an insect"}];
+             
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
                 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
                 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -12,13 +15,33 @@ var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 // Listeners
 window.onload = startGame();
 
+$(".replayBtn").on("click", function(){
+    location.reload();
+});
+
+$(".letter").click(function(){
+    checkLetter($(this).attr("id"));
+    disableButton($(this));
+});
+
+$(".hintBtn").on("click", function(){
+   showHint($(this)); 
+});
+
 
 // Functions
 function startGame(){
     pickWord();
     initBoard();
-    createLetters();
     updateBoard();
+    createLetters();
+
+}
+
+function pickWord(){
+    var randomInt = Math.floor(Math.random() * words.length);
+    selectedWord = words[randomInt].word.toUpperCase();
+    selectedHint = words[randomInt].hint;
 }
 
 function initBoard(){
@@ -27,12 +50,14 @@ function initBoard(){
     }
 }
 
-function pickWord(){
-    var randomInt = Math.floor(Math.random() * words.length);
-    selectedWord = words[randomInt];
+function showHint(){
+    $("#hint").empty();
+    $("#hint").append("<span class='hintText'>Hint: " + selectedHint + "</span>");
+    remainingGuesses -= 1;
+    updateMan();
 }
 
-// Creates the letter inside the letters div
+
 function createLetters(){
     for(var letter of alphabet){
         $("#letters").append("<button class='letter' id='" + letter + "'>" + letter + "</button>");
@@ -40,9 +65,63 @@ function createLetters(){
 }
 
 function updateBoard(){
-    for(var letter of board){
-        document.getElementById("word").innerHTML += letter + " ";
+    $("#word").empty();
+    
+    for(var i=0; i < board.length; i++){
+        $("#word").append(board[i] + " ");
+    }    
+    
+}
+
+
+function checkLetter(letter){
+    var positions = new Array();
+    
+    for (var i = 0; i < selectedWord.length; i++){
+        if(letter == selectedWord[i]){
+            positions.push(i);
+        }
+    }
+    
+    if (positions.length > 0){
+        updateWord(positions, letter)
+        
+        if(!board.includes('_')){
+            endGame(true);
+        }
+    } else {
+        remainingGuesses -= 1;
+        updateMan();
+    }
+    
+    if (remainingGuesses <=0){
+        endGame(false);
     }
 }
 
+function updateWord(positions, letter){
+    for (var pos of positions){
+        board[pos] = letter;
+    }
+    updateBoard();
+}
+
+function updateMan(){
+    $("#hangImg").attr("src", "img/stick_" + (6 - remainingGuesses) + ".png");
+}
+
+function endGame(win){
+    $("#letters").hide();
+    
+    if (win){
+        $('#won').show();
+    } else {
+        $('#lost').show();
+    }
+}
+
+function disableButton(btn){
+    btn.prop("disabled", true);
+    btn.attr("class", "btn btn-danger")
+}
 
