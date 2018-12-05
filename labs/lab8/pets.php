d172d5fd594c43f0c4699df67e954d45a807b7e8
Christian Guerrero <cguerrero@csumb.edu>
2018-12-05 17:46:31 +0000

    h

--- labs/lab8/pets.php
+++ labs/lab8/pets.php
@@ -1,13 +1,13 @@
 <?php
-
+    include 'inc/header.php';
+    
     function getPetList(){
         include 'dbConnection.php';
         $conn = getDatabaseConnection("pets");
-        
         $sql = "SELECT * FROM pets";
         $stmt = $conn->prepare($sql);
         $stmt->execute();
-        $records = $stmt->fetchAll(PDO:: FETCH_ASSOC);
+        $record = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
         return $record;
     }
@@ -21,4 +21,58 @@
         echo "<button id='" . $pet['id']."' type='button' class='btn btn-primary petLink'>Adopt Me!</button>";
         echo "<hr><br>";
     }
+?>
+
+<script>
+    $(document).ready(function(){
+        $(".petLink").click(function(){
+            
+            $('#petInfoModal').modal("show");
+            $("petInfo").html("<img src='img/loading.gif'>");
+            
+            $.ajax({
+                
+                type: "GET",
+                url: "api/getPetInfo.php",
+                dataType: "json",
+                data: {"id": $(this).attr('id')},
+                success: function(data,status){
+
+                    console.log(data);
+                    $("#petInfo").html(" <img src='img/" + data.pictureURL + "'><br>" +
+                                       " Age: " + data.age + "<br>" + 
+                                       " Breed: " + data.breeed + "<br>" + 
+                                       data.description);
+                },
+                complete: function(data,status){
+                    
+                }
+            });//ajax
+        });//.petLink click
+    });//document.ready
+</script>
+
+
+<!--Modal-->
+<div class="modal fade" id="petInfoModal" tabindex="-1" role="dialog" aria-label="Close">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="petNameModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div id="petInfo"></div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class-"btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    include 'inc/footer.php';
 ?>
