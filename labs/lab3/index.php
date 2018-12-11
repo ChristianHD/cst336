5bc46f63696c4992b7ba2c5f0fbc2e3670e395fc
Christian Guerrero <cguerrero@csumb.edu>
2018-12-11 06:34:21 +0000

    last lab

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -4,6 +4,7 @@
     
     // API call goes here
     if(isset($_GET['keyboard'])){
+        include 'api/pixabayAPI.php';
         echo "You searched for: " . $_GET['keyboard'];    
         $imageURLs = getImageURLs($keyboard);
         $backgroundImage = $imageURLs[array_rand($imageURLs)];
@@ -25,25 +26,27 @@
     </head>
     
     <body>
+        <br>
         <?php
             if(!isset($imageURLs)){
                 echo "<h2> Type a keyboard to display a slideshow <br> with random images from Pixabay.com</h2>";
             } else {
-                // Display Carousel Here
+                
         ?>
+        
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
             <!-- Indicators Here -->
             <ol class="carousel-indicators">
                 <?php
                 for ($i = 0; $i < 5; $i++){
-                    echo "<li data-targe='#carousel-example-generic' data-slide-to='$i'";
+                    echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
                     echo ($i == 0)?" class='active'": "";
                     echo "><li>";
                 }
                 ?>
             </ol>
             <!-- Wrapper for Images -->
-            <div class="carouse-inner" role="listbox">
+            <div class="carousel-inner" role="listbox">
                 <?php
                     for ($i = 0; $i < 5; $i++){
                         do {
@@ -51,10 +54,10 @@
                         }
                         while(!isset($imageURLs[$randomIndex]));
                     
-                        echo 'div class="carousel-item ';
-                        echo ($i == 0 )?"active": "";
+                        echo '<div class="item ';
+                        echo ($i == 0 )? "active" : "";
                         echo '">';
-                        echo "<img scr='" . $imageURLs[$randomIndex] . '">';
+                        echo '<img scr="' . $imageURLs[$randomIndex] . '">';
                         echo '</div>';
                         unset($imageURLs[$randomIndex]);
                     }
@@ -70,22 +73,28 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
-
-                
-        ?>
-        
-        <h1>Hello</h1>
         
         <?php
             }
         ?>
-        <br/><br />
+        <br/>
         <!-- HTML form goes here! -->
         <form>
-            <input type="text" name="keyboard" placeholder="Keyboard">
-            <input type="submit" value="Submit"/>
+            <input type="text" name="keyboard" placeholder="Keyboard" value="<?=$_GET['keyboard']?> "/>
+            <input type="radio" id="lhorizontal" name="layout" value="horizontal">
+            <label for="Horizontal"></label><label for="lhorizontal">Horizontal</label>
+            <input type="radio" id="lvertical" name="layout" value="vertical">
+             <label for="Vertical"></label><label for="lvertical">Vertical</label>
+             <select name="category">
+                 <option value="">Select One</option>
+                 <option value ="ocean">Sea</option>
+                 <option>Forest</option>
+                 <option>Mountain</option>
+                 <option>Sea</option>
+             </select>
+            <input type="submit" value="Search"/>
         </form>
-        <br/><br />
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"</script
     </body>
